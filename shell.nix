2ade693ee795a77984e323a2d7781e7ee389a294
CrepeGoat <awqatty.b@gmail.com>
2025-08-31 10:44:33 -0600

    added files to fetch dependencies & run

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{
+  pkgs ?
+    import
+      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/11cb3517b3af6af300dd6c055aeda73c9bf52c48.tar.gz")
+      { },
+}:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    nodePackages.npm
+  ];
+}
