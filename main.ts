d38759096eab611fd3ff9114af4225f0a70f7696
CrepeGoat <awqatty.b@gmail.com>
2025-09-01 19:11:13 -0600

    obsidian plugin tutorial step 5: rename things https://docs.obsidian.md/Plugins/Getting+started/Build+a+plugin#Step+5+Update+the+source+code

--- main.ts
+++ main.ts
@@ -9,18 +9,16 @@ import {
 	Setting,
 } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface FastMatherSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: FastMatherSettings = {
 	mySetting: "default",
 };
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FastMather extends Plugin {
+	settings: FastMatherSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -126,9 +124,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: FastMather;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: FastMather) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
