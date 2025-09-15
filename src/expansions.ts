e8f9a2598cf213d4ac2b9f3101563a8390f4ce41
CrepeGoat <awqatty.b@gmail.com>
2025-09-15 10:13:36 -0600

    wip

---
+++ src/expansions.ts
@@ -0,0 +1,64 @@
+// ref: https://www.math.toronto.edu/mathit/symbols-letter.pdf
+
+const greek_letters = [
+	"alpha",
+	"theta ",
+	"tau",
+	"beta ",
+	"vartheta",
+	"pi",
+	"upsilon",
+	"gamma",
+	"iota",
+	"varpi",
+	"phi",
+	"delta",
+	"kappa",
+	"rho",
+	"varphi",
+	"epsilon",
+	"lambda",
+	"varrho",
+	"chi",
+	"varepsilon",
+	"mu",
+	"sigma",
+	"psi",
+	"zeta",
+	"nu",
+	"varsigma",
+	"omega",
+	"eta",
+	"xi",
+];
+
+const symbols = [
+	"bot",
+	"forall",
+	"imath",
+	"ni",
+	"notni",
+	"top",
+	"ell",
+	"hbar",
+	"in",
+	"notin",
+	"partial",
+	"wp",
+	"exists",
+	"nexists",
+	"Im",
+	"jmath",
+	"Re",
+];
+
+const fourier = ["partial", "varpartialdiff"];
+
+const delimiters = [
+	"lcorners",
+	"rcorners",
+	"ulcorner",
+	"llcorner",
+	"urcorner",
+	"lrcorner",
+];
